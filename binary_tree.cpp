5ad4a56be4053dccbc36c126b77970892a03f658
Nandan Pandey <pandeynandancse@gmail.com>
2020-12-09 19:41:17 -0800

    Update binary_tree.cpp

--- binary_tree.cpp
+++ binary_tree.cpp
@@ -30,3 +30,54 @@
     
  --------------------------------------------------------------------------------------------------------------------------------------------------------------------
     
+3) Catalan Number DP : 
+unsigned long int catalanDP(unsigned int n) 
+{ 
+    unsigned long int catalan[n + 1]; 
+    catalan[0] = catalan[1] = 1; 
+    for (int i = 2; i <= n; i++)  
+    { 
+        catalan[i] = 0; 
+        for (int j = 0; j < i; j++) 
+            catalan[i] += catalan[j]  
+                       * catalan[i - j - 1]; 
+    } 
+    return catalan[n]; 
+} 
+
+
+4)Catalan Number  Recursive Number
+  unsigned long int catalan(unsigned int n) 
+{  
+    if (n <= 1) 
+        return 1; 
+    unsigned long int res = 0; 
+    for (int i = 0; i < n; i++) 
+        res += catalan(i)  
+            * catalan(n - i - 1); 
+    return res; 
+} 
+
+
+5) Catalan Number  Coefficient Method
+unsigned long int binomialCoeff(unsigned int n, 
+                                unsigned int k) 
+{ 
+    unsigned long int res = 1; 
+    if (k > n - k) 
+        k = n - k; 
+  // Calculate value of [n*(n-1)*---*(n-k+1)] / 
+    // [k*(k-1)*---*1] 
+    for (int i = 0; i < k; ++i) { 
+        res *= (n - i); 
+        res /= (i + 1); 
+    } 
+    return res; 
+} 
+unsigned long int catalan(unsigned int n) 
+{ 
+    unsigned long int c = binomialCoeff(2 * n, n); 
+    return c / (n + 1); 
+}
+
+------------------------------------------------------------------------------------------------------------------------------------------------------------------------
