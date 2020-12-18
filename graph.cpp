a1cfb49a2783f8478793978444f3218ec7991601
Nandan Pandey <pandeynandancse@gmail.com>
2020-12-18 10:07:51 -0800

    Update graph.cpp

--- graph.cpp
+++ graph.cpp
@@ -30,6 +30,266 @@
                     
                     
                     
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+2. DFS of Graph
+
+
+
+
+
+
+
+3. BFS of graph
+
+
+
+
+
+
+4. Prims algorithm > adjacency matrix > not efficient
+
+
+
+
+
+
+// A C++ program for Prim's Minimum  
+// Spanning Tree (MST) algorithm. The program is  
+// for adjacency matrix representation of the graph  
+#include <bits/stdc++.h> 
+using namespace std; 
+  
+// Number of vertices in the graph  
+#define V 5  
+  
+// A utility function to find the vertex with  
+// minimum key value, from the set of vertices  
+// not yet included in MST  
+int minKey(int key[], bool mstSet[])  
+{  
+    // Initialize min value  
+    int min = INT_MAX, min_index;  
+  
+    for (int v = 0; v < V; v++)  
+        if (mstSet[v] == false && key[v] < min)  
+            min = key[v], min_index = v;  
+  
+    return min_index;  
+}  
+  
+// A utility function to print the  
+// constructed MST stored in parent[]  
+void printMST(int parent[], int graph[V][V])  
+{  
+    cout<<"Edge \tWeight\n";  
+    for (int i = 1; i < V; i++)  
+        cout<<parent[i]<<" - "<<i<<" \t"<<graph[i][parent[i]]<<" \n";  
+}  
+  
+// Function to construct and print MST for  
+// a graph represented using adjacency  
+// matrix representation  
+void primMST(int graph[V][V])  
+{  
+    // Array to store constructed MST  
+    int parent[V];  
+      
+    // Key values used to pick minimum weight edge in cut  
+    int key[V];  
+      
+    // To represent set of vertices included in MST  
+    bool mstSet[V];  
+  
+    // Initialize all keys as INFINITE  
+    for (int i = 0; i < V; i++)  
+        key[i] = INT_MAX, mstSet[i] = false;  
+  
+    // Always include first 1st vertex in MST.  
+    // Make key 0 so that this vertex is picked as first vertex.  
+    key[0] = 0;  
+    parent[0] = -1; // First node is always root of MST  
+  
+    // The MST will have V vertices  
+    for (int count = 0; count < V - 1; count++) 
+    {  
+        // Pick the minimum key vertex from the  
+        // set of vertices not yet included in MST  
+        int u = minKey(key, mstSet);  
+  
+        // Add the picked vertex to the MST Set  
+        mstSet[u] = true;  
+  
+        // Update key value and parent index of  
+        // the adjacent vertices of the picked vertex.  
+        // Consider only those vertices which are not  
+        // yet included in MST  
+        for (int v = 0; v < V; v++)  
+  
+            // graph[u][v] is non zero only for adjacent vertices of m  
+            // mstSet[v] is false for vertices not yet included in MST  
+            // Update the key only if graph[u][v] is smaller than key[v]  
+            if (graph[u][v] && mstSet[v] == false && graph[u][v] < key[v])  
+                parent[v] = u, key[v] = graph[u][v];  
+    }  
+  
+    // print the constructed MST  
+    printMST(parent, graph);  
+}  
+  
+// Driver code 
+int main()  
+{  
+    /* Let us create the following graph  
+        2 3  
+    (0)--(1)--(2)  
+    | / \ |  
+    6| 8/ \5 |7  
+    | / \ |  
+    (3)-------(4)  
+            9     */
+    int graph[V][V] = { { 0, 2, 0, 6, 0 },  
+                        { 2, 0, 3, 8, 5 },  
+                        { 0, 3, 0, 0, 7 },  
+                        { 6, 8, 0, 0, 9 },  
+                        { 0, 5, 7, 9, 0 } };  
+  
+    // Print the solution  
+    primMST(graph);  
+  
+    return 0;  
+}  
+  
+
+
+
+
+5. Dijkastras Algorithm > adjancency matrix  > not efficient
+
+
+
+
+
+// A C++ program for Dijkstra's single source shortest path algorithm. 
+// The program is for adjacency matrix representation of the graph 
+
+#include <limits.h> 
+#include <stdio.h> 
+
+// Number of vertices in the graph 
+#define V 9 
+
+// A utility function to find the vertex with minimum distance value, from 
+// the set of vertices not yet included in shortest path tree 
+int minDistance(int dist[], bool sptSet[]) 
+{ 
+	// Initialize min value 
+	int min = INT_MAX, min_index; 
+
+	for (int v = 0; v < V; v++) 
+		if (sptSet[v] == false && dist[v] <= min) 
+			min = dist[v], min_index = v; 
+
+	return min_index; 
+} 
+
+// A utility function to print the constructed distance array 
+void printSolution(int dist[]) 
+{ 
+	printf("Vertex \t\t Distance from Source\n"); 
+	for (int i = 0; i < V; i++) 
+		printf("%d \t\t %d\n", i, dist[i]); 
+} 
+
+// Function that implements Dijkstra's single source shortest path algorithm 
+// for a graph represented using adjacency matrix representation 
+void dijkstra(int graph[V][V], int src) 
+{ 
+	int dist[V]; // The output array. dist[i] will hold the shortest 
+	// distance from src to i 
+
+	bool sptSet[V]; // sptSet[i] will be true if vertex i is included in shortest 
+	// path tree or shortest distance from src to i is finalized 
+
+	// Initialize all distances as INFINITE and stpSet[] as false 
+	for (int i = 0; i < V; i++) 
+		dist[i] = INT_MAX, sptSet[i] = false; 
+
+	// Distance of source vertex from itself is always 0 
+	dist[src] = 0; 
+
+	// Find shortest path for all vertices 
+	for (int count = 0; count < V - 1; count++) { 
+		// Pick the minimum distance vertex from the set of vertices not 
+		// yet processed. u is always equal to src in the first iteration. 
+		int u = minDistance(dist, sptSet); 
+
+		// Mark the picked vertex as processed 
+		sptSet[u] = true; 
+
+		// Update dist value of the adjacent vertices of the picked vertex. 
+		for (int v = 0; v < V; v++) 
+
+			// Update dist[v] only if is not in sptSet, there is an edge from 
+			// u to v, and total weight of path from src to v through u is 
+			// smaller than current value of dist[v] 
+			if (!sptSet[v] && graph[u][v] && dist[u] != INT_MAX 
+				&& dist[u] + graph[u][v] < dist[v]) 
+				dist[v] = dist[u] + graph[u][v]; 
+	} 
+
+	// print the constructed distance array 
+	printSolution(dist); 
+} 
+
+// driver program to test above function 
+int main() 
+{ 
+	/* Let us create the example graph discussed above */
+	int graph[V][V] = { { 0, 4, 0, 0, 0, 0, 0, 8, 0 }, 
+						{ 4, 0, 8, 0, 0, 0, 0, 11, 0 }, 
+						{ 0, 8, 0, 7, 0, 4, 0, 0, 2 }, 
+						{ 0, 0, 7, 0, 9, 14, 0, 0, 0 }, 
+						{ 0, 0, 0, 9, 0, 10, 0, 0, 0 }, 
+						{ 0, 0, 4, 14, 10, 0, 2, 0, 0 }, 
+						{ 0, 0, 0, 0, 0, 2, 0, 1, 6 }, 
+						{ 8, 11, 0, 0, 0, 0, 1, 0, 7 }, 
+						{ 0, 0, 2, 0, 0, 0, 6, 7, 0 } }; 
+
+	dijkstra(graph, 0); 
+
+	return 0; 
+} 
+
+
+
+
+
+
+
+
+
+
+
+
+6. Bellman Fors Algorithm
+
+
+
+
+
                     
                     
                     
