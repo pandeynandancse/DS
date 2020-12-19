af44ed0572a5a6bdee868a0d5e321f25b12af124
Nandan Pandey <pandeynandancse@gmail.com>
2020-12-19 10:00:11 -0800

    Update bit-manipulation.cpp

--- bit-manipulation.cpp
+++ bit-manipulation.cpp
@@ -80,3 +80,107 @@ public:
                                             bitset<32>a(res);                     //for(int i=0;i<32;i++){ cout<<a[i]; } //10100000000000000000000000000000
                                             return a.count();
                                         }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+6. These are four important built-in functions in GCC compiler:
+    Similarly you can use __builtin_popcount(x) , __builtin_popcountl(x) & __builtin_popcountll(x) for long and long long data types.  >  This function is used to count the number of one’s(set bits) in an integer.
+    Similarly you can use __builtin_parity(x) , __builtin_parityl(x) & __builtin_parityll(x) for long and long long data types. >   This function returns true(1) if the number has odd parity else it returns false(0) for even parity.
+    Similarly you can use __builtin_clz(x) , __builtin_clzl(x) & __builtin_clzll(x) for long and long long data types.    >   This function is used to count the leading zeros of the integer.   
+     Similarly you can use __builtin_ctz(x) , __builtin_ctzl(x) & __builtin_ctzll(x) for long and long long data types.
+       
+       
+
+       
+       
+7.  sort(arr, arr + n, greater<int>());             // descending order
+
+
+bool compareInterval(Interval i1, Interval i2)
+{
+    return (i1.start < i2.start);
+}
+int main()
+{
+    Interval arr[] = { { 6, 8 }, { 1, 9 }, { 2, 4 }, { 4, 7 } };
+    int n = sizeof(arr) / sizeof(arr[0]);
+    sort(arr, arr + n, compareInterval);
+    for (int i = 0; i < n; i++)
+        cout << "[" << arr[i].start << "," << arr[i].end << "] ";
+    return 0;
+}
+
+
+
+
+
+
+8. Sort Integers by The Number of 1 Bits
+
+vector<int> sortByBits(vector<int>& arr) {
+        sort(arr.begin(), arr.end(), [](int a, int b){
+                        int x = __builtin_popcount(a);
+                        int y = __builtin_popcount(b);
+                        return x==y ? a<b : x<y;                   
+        });
+            for(int i=0; i<arr.size(); i++){
+                cout<<arr[i]<<" ";
+            }
+        return arr;
+    }
+    
+
+
+
+
+
+
+
+9. no. of set bits   >   alternate of builtin-popcount
+int setbits(int n) {
+        int setbit = 0;
+        while (n) {
+            n &= n-1;
+            ++setbit;
+        }
+        return setbit;
+    }
+    
+
+
+
+
+
+
+10.
+   int findComplement(int num) { 
+	int numberOfBits = (int)log2(num) + 1;
+	return pow(2,numberOfBits) - num - 1;
+}
+
+
+
+
+11. even number  > if( (num & 1) == 0))
+   
+   
+   
+12.      num=5
+        bitset<32> a(num);            //a = 10100000000000000000000000000000
+         a.to_ulong();            // 5
