9b85470d87a110c4aee676e32ec547146b966441
Nandan Pandey <pandeynandancse@gmail.com>
2021-03-05 21:26:34 -0800

    Update bit-manipulation.cpp

--- bit-manipulation.cpp
+++ bit-manipulation.cpp
@@ -285,4 +285,23 @@ bool is_prime(int a){
 
 
 
-
+17. https://www.geeksforgeeks.org/length-longest-consecutive-1s-binary-representation/
+			    
+			    int maxConsecutiveOnes(int x) 
+{ 
+    // Initialize result 
+    int count = 0; 
+  
+    // Count the number of iterations to 
+    // reach x = 0. 
+    while (x!=0) 
+    { 
+        // This operation reduces length 
+        // of every sequence of 1s by one. 
+        x = (x & (x << 1)); 
+  
+        count++; 
+    } 
+  
+    return count; 
+} 
